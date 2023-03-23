c07f9c374868e7914dab78536d0f553d9e57c563
Alan Lima <alan.silva1f@gmail.com>
2023-03-23 00:56:54 -0300

    Colocando menu busca

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const inputSearchPokemon = document.querySelector("#search_Pokemon");
 
 const generatePokemonPromises = () => Array(150).fill().map((_, index) => {
     return fetch(getPokemonUrl(index + 1)).then(response => response.json());
@@ -10,7 +11,7 @@ const generateHTML = pokemons => pokemons.reduce((acumalador, { name, id, types 
     acumalador += `
             <li class="card ${ElementTypes[0]}">
                 <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
-                <h2 class="card-title">${id}. ${name}</h2>
+                <h2 class="card-title">${id}. <span class="nome_pokemon">${name}</span></h2>
                 <p class="card-subtitle">${ElementTypes.join(" | ")}</p>
             </li>`;
 
@@ -19,7 +20,7 @@ const generateHTML = pokemons => pokemons.reduce((acumalador, { name, id, types 
 
 const insertPokemonsIntoPage = pokemons => {
     const ul = document.querySelector('[data-js="pokedex"]');
-    ul.innerHTML = pokemons
+    ul.innerHTML = pokemons;
 }
 
 const pokemonPromises = generatePokemonPromises();
@@ -27,6 +28,31 @@ const pokemonPromises = generatePokemonPromises();
 Promise.all(pokemonPromises)
     .then(generateHTML)
     .then(insertPokemonsIntoPage)
+
+const printPokemonScreen = inputValue => card =>{
+    const pokemon = card.querySelector(".nome_pokemon").textContent.toLocaleLowerCase();
+
+    const cardContainsInputValue = pokemon.includes(inputValue);
+
+    if(cardContainsInputValue){
+        card.style.display = 'block';
+        return
+    }
+
+    card.style.display = 'none';
+}
+
+const filterPokemon = e =>{
+    e.preventDefault();
+
+    let searchPokemon = e.target.value.toLocaleLowerCase();
+    
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(printPokemonScreen(searchPokemon))
+}
+
+inputSearchPokemon.addEventListener("input", filterPokemon);
 
 // const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
