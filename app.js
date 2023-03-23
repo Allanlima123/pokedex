8c5d30c1044a312bb26d49f8c1879a0833f19f0a
Alan Lima <alan.silva1f@gmail.com>
2023-03-23 01:04:35 -0300

    Colocando menu busca

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 const inputSearchPokemon = document.querySelector("#search_Pokemon");
 
-const generatePokemonPromises = () => Array(150).fill().map((_, index) => {
-    return fetch(getPokemonUrl(index + 1)).then(response => response.json());
-});
+const generatePokemonPromises = () =>
+  Array(150)
+    .fill()
+    .map((_, index) => {
+      return fetch(getPokemonUrl(index + 1)).then((response) =>
+        response.json()
+      );
+    });
 
-const generateHTML = pokemons => pokemons.reduce((acumalador, { name, id, types }) => {
-    const ElementTypes = types.map(typeInfo => typeInfo.type.name);
+const generateHTML = (pokemons) =>
+  pokemons.reduce((acumalador, { name, id, types }) => {
+    const ElementTypes = types.map((typeInfo) => typeInfo.type.name);
 
     acumalador += `
             <li class="card ${ElementTypes[0]}">
@@ -15,42 +21,42 @@ const generateHTML = pokemons => pokemons.reduce((acumalador, { name, id, types 
                 <p class="card-subtitle">${ElementTypes.join(" | ")}</p>
             </li>`;
 
-    return acumalador
-}, "");
+    return acumalador;
+  }, "");
 
-const insertPokemonsIntoPage = pokemons => {
-    const ul = document.querySelector('[data-js="pokedex"]');
-    ul.innerHTML = pokemons;
-}
+const insertPokemonsIntoPage = (pokemons) => {
+  const ul = document.querySelector('[data-js="pokedex"]');
+  ul.innerHTML = pokemons;
+};
 
 const pokemonPromises = generatePokemonPromises();
 
-Promise.all(pokemonPromises)
-    .then(generateHTML)
-    .then(insertPokemonsIntoPage)
+Promise.all(pokemonPromises).then(generateHTML).then(insertPokemonsIntoPage);
 
-const printPokemonScreen = inputValue => card =>{
-    const pokemon = card.querySelector(".nome_pokemon").textContent.toLocaleLowerCase();
+const printPokemonScreen = (inputValue) => (card) => {
+  const pokemon = card
+    .querySelector(".nome_pokemon")
+    .textContent.toLocaleLowerCase();
 
-    const cardContainsInputValue = pokemon.includes(inputValue);
+  const cardContainsInputValue = pokemon.includes(inputValue);
 
-    if(cardContainsInputValue){
-        card.style.display = 'block';
-        return
-    }
+  if (cardContainsInputValue) {
+    card.style.display = "block";
+    return;
+  }
 
-    card.style.display = 'none';
-}
+  card.style.display = "none";
+};
 
-const filterPokemon = e =>{
-    e.preventDefault();
+const filterPokemon = (e) => {
+  e.preventDefault();
 
-    let searchPokemon = e.target.value.toLocaleLowerCase();
-    
-    const cards = document.querySelectorAll(".card");
+  let searchPokemon = e.target.value.toLocaleLowerCase();
 
-    cards.forEach(printPokemonScreen(searchPokemon))
-}
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(printPokemonScreen(searchPokemon));
+};
 
 inputSearchPokemon.addEventListener("input", filterPokemon);
 
@@ -84,7 +90,6 @@ inputSearchPokemon.addEventListener("input", filterPokemon);
 // // Promise.all(pokemonPromises)
 // //     .then(generateHTML)
 // //     .then(insertPokemonsIntoPage)
-
 
 // async function getDatePokemons() {
 //     let pokemonPromises = await Promise.all(await generatePokemonPromises())
